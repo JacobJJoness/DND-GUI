009ed67368bc4d80b8ba5da4d0cdf1f9c81dd03c
JacobJJoness <genjacob20@gmail.com>
2022-10-18 21:23:26 -0500

    Initial Commit
    Code is currently inoperable

---
+++ character.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Random;
+
+class character {
+
+    //Hashtable dictionary data structure
+    Hashtable<String, String> statsDict = new Hashtable<String, String>();
+    private String name;
+    private String description;
+
+    //randomized character constructor W.I.P
+    character(){
+        name="Ragnar";
+        description="Empty";
+    }
+}
