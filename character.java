c1f52399e5b8661d8b496c2dc900a974ba5f13ed
JacobJJoness <genjacob20@gmail.com>
2022-10-18 21:42:06 -0500

    General Class Setup
    added a stat generator function, also  getters and setters

--- character.java
+++ character.java
@@ -8,9 +8,40 @@ class character {
     private String name;
     private String description;
 
-    //randomized character constructor W.I.P
+    //randomized character constructor current a W.I.P
     character(){
-        name="Ragnar";
-        description="Empty";
+        this.name="Ragnar";
+        this.description="Empty";
+
+        //inserting elements into the dictionary
+        statsDict.put("INT","20");
+        statsDict.put("DEX","20");
+        statsDict.put("STR","20");
+        statsDict.put("CON","20");
+        statsDict.put("CHR","20");
+        statsDict.put("WIS","20");
+    }
+
+    private String statGenerator(){
+        Random rand = new Random();//new random object
+        int upperLimit = 20;//initializing a upperlimit value
+        int generatedStat = rand.nextInt(upperLimit);
+        String stringStat = String.valueOf(generatedStat);
+        return stringStat;
+
+    }
+    // GETTERS and SETTERS below
+
+    public String getName(){
+        return this.name;
+    }
+    public String getDesc(){
+        return this.description;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public void setDesc(String newDesc){
+        this.description = newDesc;
     }
 }
