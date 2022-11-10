6211e58536e5298efeb27b4ba43127a01ec2f8cc
JacobJJoness <genjacob20@gmail.com>
2022-11-09 22:02:51 -0600

    Updated GUI Aesthetic
    The GUI is now starting to look appealing to the eye.

--- GUI.java
+++ GUI.java
@@ -19,12 +19,16 @@ import java.awt.FlowLayout;
 
 class Frame extends JFrame implements ActionListener{
     private character mainChara = new character();
+
     //initializing buttons
     JButton generator;
+
     //initializing panels
     JPanel charaPanel;
     JPanel genPanel;
     JPanel infoPanel;
+
+
     //initializing labels
     JLabel intStatDisp;
     JLabel dexStatDisp;
@@ -56,11 +60,10 @@ class Frame extends JFrame implements ActionListener{
         ImageIcon backImg = new ImageIcon("public/background.jpg");
         JLabel background = new JLabel("",backImg,JLabel.CENTER);
         background.setBounds(0,0,1920,1080);
-        background.setLayout(new BorderLayout());
+        background.setLayout(new FlowLayout());
         
-      
         add(background);
-        
+
         
         
 
@@ -77,9 +80,11 @@ class Frame extends JFrame implements ActionListener{
     private void generatorPanel(JLabel item){
         //panel structure and style
         genPanel = new JPanel();
-        genPanel.setBackground(Color.blue);
+        
+        
         genPanel.setBounds(0,0,250,250);
-        genPanel.setPreferredSize(new Dimension(250,250));
+        genPanel.setPreferredSize(new Dimension(300,300));
+
         //Button for character generation
         generator = new JButton();
         generator.setText("Generate");//text inside button
@@ -87,36 +92,51 @@ class Frame extends JFrame implements ActionListener{
         generator.setFocusable(false);//removing a default box around text
         genPanel.add(generator);//adding button to panel
 
+
+        //code to wrap panel to make it partially transparent
+        genPanel.setBackground(new Color(100,100,100,150));//A value determines transparency
+        item.add(new AlphaContainer(genPanel));
         
-        item.add(genPanel);
     }
+
+
     //may be used to give the user a Avatar for the character of sorts
     private void characterImagePanel(JLabel item){
         charaPanel = new JPanel();
-        charaPanel.setBackground(Color.red);
+
         charaPanel.setBounds(250,0,250,250);
         charaPanel.setPreferredSize(new Dimension(250,250));//setting to a preference size, this is due to the fact that Jpanels will automatically just fill around components
-        item.add(charaPanel);
+        //code to wrap panel to make it partially transparent
+        charaPanel.setBackground(new Color(100,100,100,150));//A value determines transparency
+        item.add(new AlphaContainer(charaPanel));
     }
+
+
     //should display character information, stats,description,etc
     private void informationPanel(JLabel item){
         infoPanel = new JPanel();
         infoPanel.setLayout(new FlowLayout());
-        infoPanel.setBackground(Color.green);
+        
         infoPanel.setBounds(0,250,500,250);
         infoPanel.setPreferredSize(new Dimension(250,250));//setting to a preference size, this is due to the fact that Jpanels will automatically just fill around components
         
+        //adding statistic labels
         setStatLabels(infoPanel);//adding generated stats
-        
-       
 
-        
-
-        item.add(infoPanel);
+        //code to wrap panel to make it partially transparent
+        infoPanel.setBackground(new Color(100,100,100,150));//A value determines transparency
+        item.add(new AlphaContainer(infoPanel));
 
     }
+    
+    
+    
+
+
+
     private void setStatLabels(JPanel item){
-         
+        
+        //storing then accessing stats
         Hashtable<String, String> stats = mainChara.getStats(); //grabbing character stats
 
         intStatDisp = new JLabel();
@@ -137,6 +157,7 @@ class Frame extends JFrame implements ActionListener{
         wisStatDisp = new JLabel();
         wisStatDisp.setText("Wis: " + stats.get("WIS"));
 
+        //adding all items
         item.add(intStatDisp);
         item.add(dexStatDisp);
         item.add(conStatDisp);
@@ -146,20 +167,18 @@ class Frame extends JFrame implements ActionListener{
     }
 
     private void updateStatLabels(){
+        //accessing stat labels and updating when generate is used
         Hashtable<String, String> stats = mainChara.getStats(); 
         intStatDisp.setText("Int : " + stats.get("INT"));//setting character stat to display generated stats
+        
         dexStatDisp.setText("Dex : " + stats.get("DEX"));
 
-        
         conStatDisp.setText("Str: " + stats.get("STR"));
 
-    
         chrStatDisp.setText("CON: " + stats.get("CON"));
 
-      
         strStatDisp.setText("Chr: " + stats.get("CHR"));
 
-       
         wisStatDisp.setText("Wis: " + stats.get("WIS"));
     }
    
@@ -171,8 +190,11 @@ class Frame extends JFrame implements ActionListener{
   
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+        // If we use checks similar to  the one below we may use multiple buttons to do 
+        //various tasks
+
         if(e.getSource()==generator){
+
             mainChara.newCharacter();
             updateStatLabels();
             
