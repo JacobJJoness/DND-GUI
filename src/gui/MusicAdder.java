539764acbe746660c5185ad2baafab016bdac8e4
Ila Walace <115182946+ila-w@users.noreply.github.com>
2022-11-30 19:50:44 -0600

    music adder file javadoc complete

--- src/gui/MusicAdder.java
+++ src/gui/MusicAdder.java
@@ -10,46 +10,31 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  *The MusicAdder class adds the background music to wherever it is 
- * Instantiated. As long as the program continues to run it will
- * play music
+ * Instantiated. As long as the program continues to run, it will
+ * play music.
+ * 
  * @author Jacob Jones 
  * @since November 28, 2022
  */
 public class MusicAdder {
     
-
-    MusicAdder() throws UnsupportedAudioFileException, IOException, LineUnavailableException
-    {
-
-
+    /**
+     * Default constructor for the MusicAdder object, 
+     * which creates a music loop for the application.
+     * 
+     * @throws UnsupportedAudioFileException if wrong file type
+     * @throws IOException if IO error occurs while playing
+     * @throws LineUnavailableException if no line available 
+     */
+    MusicAdder() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        //creating soundfile clip
         File soundFile = new File("music/soundtrack.wav");
         AudioInputStream soundStream = AudioSystem.getAudioInputStream(soundFile);
         Clip fullClip = AudioSystem.getClip();
   
+        //play and loop soundfile clip forever while application continues to run
         fullClip.open(soundStream);
         fullClip.start();
         fullClip.loop(Clip.LOOP_CONTINUOUSLY);
-        
-        
-    
-       
-    }
-}
-
-
-class TestMusic{
-    public static void main(String[] args) 
-    {
-        try
-        {
-            MusicAdder music = new MusicAdder();
-            System.out.println("success");
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-            System.out.println("failed");
-
-        }
     }
 }
